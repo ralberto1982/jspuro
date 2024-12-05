2c781d1f09fa852d63c20e3baec340525c3ce3a4
Alberto R <ralberto2312@gmail.com>
2024-12-05 18:50:27 -0500

    objetos.1

---
+++ 07-Objetos/js/01-app.js
@@ -0,0 +1,10 @@
+// OBJETOS  un objeto agrupa todo en una sola variable
+
+// Objeto literal
+const producto = {
+    nombre:'edwin',
+    precio:300,
+    disponible:true
+}
+
+console.log(producto)
