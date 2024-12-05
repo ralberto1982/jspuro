83eba5c241c3f4c2da28ddd71a3198a594ff3dbf
Alberto R <ralberto2312@gmail.com>
2024-12-05 17:46:04 -0500

    objetos

--- 05-Operadores/js/01-app.js
+++ 05-Operadores/js/01-app.js
@@ -2,5 +2,15 @@
 const numero1=10
 const numero2=15
 
+// MAYOR A >
+// MENOR A <
+// ASIGNANDO =
+// IGUAL A ==
+// IDENTICO A ===
+// DIFERNTE !=
+// OR |
+// Y & 
+// NULL
+// UNDEFINE
 console.log(numero1> numero2)
 
---
+++ 06-Booleans/js/01-app.js
@@ -0,0 +1,8 @@
+// BOLEAN SOLO TIENE 2 VALORES TRUE OR FALSE
+
+const bolena=true;
+const boleano=false;
+const autenticado = true;
+
+// operador ternario
+console.log(autenticado ? 'si esta ':'no esta')
